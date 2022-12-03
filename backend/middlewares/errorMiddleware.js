f4dc8895507c4ecf077f2bd3d41e7bbaa0c9015f
KAZOUM Abdelaziz <abdukazoum@gmail.com>
2022-12-03 17:23:40 +0100

    fix the error handler bug | done

--- backend/middlewares/errorMiddleware.js
+++ backend/middlewares/errorMiddleware.js
@@ -1,14 +1,12 @@
 const notFound = (req, res, next) => {
   const error = new Error(`Not Found - ${req.originalUrl}`);
-  console.log("hello" + error);
   res.status(404);
   next(error);
 };
 
 const errorHandler = (err, req, res, next) => {
-  const statusCode = err.statusCode === 200 ? 500 : err.statusCode;
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
 
-  console.log(err.statusCode);
   res.status(statusCode);
   res.json({
     message: err.message,
--- backend/routes/productsRoute.js
+++ backend/routes/productsRoute.js
@@ -1,27 +1,26 @@
 import express from "express";
 import products from "../data/productsList.js";
 import Product from "../models/productModel.js";
+import asyncHandler from "express-async-handler";
 
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  Product.find()
-    .then((pro) => {
-      res.status(200).json(pro);
-    })
-    .catch((err) => {
-      res.status(404).json({ message: err });
-    });
-});
+// get all products :
+router.get(
+  "/",
+  asyncHandler(async (req, res, next) => {
+    const pro = await Product.find();
+    res.json(pro);
+  })
+);
 
-router.get("/:id", (req, res, next) => {
-  Product.findById({ _id: req.params.id })
-    .then((proItem) => {
-      res.status(200).json(proItem);
-    })
-    .catch((err) => {
-      res.status(404).json({ message: err });
-    });
-});
+// Search for Producxt by id :
+router.get(
+  "/:id",
+  asyncHandler(async (req, res, next) => {
+    const proItem = await Product.findById({ _id: req.params.id });
+    res.json(proItem);
+  })
+);
 
 export default router;
